00bfcf7a06bc40aaed13a7a2f65c149ce91fa501
miha11-hash <d_mihaela@msn.com>
2023-09-26 12:30:40 +0300

    anvbar syle moverd to styled-components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import { IoCloseOutline } from "react-icons/io5";
 import { HiMenuAlt3 } from "react-icons/hi";
 import { Link } from "react-scroll";
 import { useGlobalContext } from "../useContext";
+import styled from "styled-components";
 
 const Navbar = () => {
   let { isSidebar, setSidebar } = useGlobalContext();
@@ -24,7 +25,7 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav className={`navbar section-center ${isNavbarVisible ? "" : "sticky"}`}>
+    <Wrapper className={`navbar section-center ${isNavbarVisible ? "" : "sticky"}`}>
       <div className="logo">
         <h2>Logo</h2>
       </div>
@@ -81,8 +82,127 @@ const Navbar = () => {
           )}
         </button>
       </div>
-    </nav>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.nav`
+  height: 5rem;
+  width: 100%;
+  background: var(--clr-white);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 3rem;
+  position: sticky;
+  top: 0;
+  z-index: 999;
+  transition: top 0.5s ease-in-out;
+  border-bottom: 2px solid black;
+
+  .sticky {
+    top: 20px;
+  }
+
+  div.logo {
+    flex: 1;
+  }
+
+  div.nav-links {
+    display: flex;
+    flex: 4;
+    align-items: center;
+    justify-content: end;
+  }
+
+  div.nav-links ul.links {
+    display: flex;
+    border: 2px solid red;
+  }
+
+  div.nav-links ul.links a {
+    font-size: 1.2rem;
+    display: inline-block;
+    margin-right: 1.5rem;
+    cursor: pointer;
+    color: var(--clr-primary-1);
+  }
+
+  button {
+    display: none;
+  }
+
+  @media only screen and (max-width: 992px) {
+    .nav-links ul {
+      display: none;
+    }
+    button {
+      display: inline-block;
+    }
+  }
+
+  a.active {
+    color: red;
+  }
+
+  .icon-btn {
+    border: none;
+    outline: none;
+    font-size: 1.8rem;
+    background: transparent;
+    cursor: pointer;
+    padding: 0.5rem;
+    border-radius: 0.3rem;
+    transition: background 0.5se ease;
+  }
+
+  .icon-btn:hover {
+    background: var(--clr-grey-9);
+  }
+///last
+  ul.links li a {
+    display: block;
+    position: relative;
+    padding: 5px;
+    font-size: 18px;
+    font-family: sans-serif;
+    color: #fff;
+    text-transform: uppercase;
+    transition: 0.5s;
+  }
+
+  ul.links:hover li a {
+    transform: scale(1.5);
+    opacity: 0.2;
+    filter: blur(5px);
+  }
+
+  ul.links li a:hover {
+    transform: scale(1.7);
+    opacity: 1;
+    filter: blur(0);
+    color: #fff;
+  }
+
+  ul.links li a:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: #ff497c;
+    transition: 0.5s;
+    transform-origin: right;
+    transform: scaleX(0);
+    z-index: -1;
+  }
+
+  ul.links li a:hover:before {
+    transition: transform 0.5s;
+    transform-origin: left;
+    transform: scaleX(1);
+  }
+`;
 
 export default Navbar;
--- src/data.js
+++ src/data.js
@@ -1,8 +1,80 @@
+//icons title
+import {GiWinterHat} from "react-icons/gi";
+import {BsFillHouseGearFill} from "react-icons/bs";
+import {BsGithub} from "react-icons/bs";
+import {GiReceiveMoney} from "react-icons/gi";
+import {GiWoodCabin} from "react-icons/gi";
+import {IoIosPeople} from "react-icons/io";
+import {GiRose} from "react-icons/gi";
+import {GiCutLemon} from "react-icons/gi";
+//icons technologies
+
+
+
 export const data = [
-  { id: 1, title: "Winter Landing Page", type: "Websites", icon: "", url: "", description: ""},
-  { id: 2, title: "Architect Design", type: "Websites", icon: "", url:"", description: ""},
-  { id: 3, title: "GitHub Finder", type: "Web-apps", icon: "", url:"", description: ""},
-  { id: 4, title: "NPV and Risk Assessment", type: "Web-apps", icon: "", url:"", description: ""},
-  { id: 5, title: "E-commerce", type: "Front-end", icon: "", url:"", description: ""},
-  { id: 6, title: "Jobster", type: "Full-stack", icon: "", url:"", description: ""},
+  {
+    id: 1,
+    title: "Winter Landing Page",
+    type: "Websites",
+    icon: <GiWinterHat></GiWinterHat>,
+    url: "",
+    description: "https://winter-beauties.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Architect Design",
+    type: "Websites",
+    icon: <BsFillHouseGearFill></BsFillHouseGearFill>,
+    url: "",
+    description: "https://interior-design-com.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "GitHub Finder",
+    type: "Web-apps",
+    icon: <BsGithub></BsGithub>,
+    url: "https://github-finder-mdcodelab.netlify.app/",
+    description: "",
+  },
+  {
+    id: 4,
+    title: "NPV and Risk Assessment",
+    type: "Web-apps",
+    icon: <GiReceiveMoney></GiReceiveMoney>,
+    url: "",
+    description: "",
+  },
+  {
+    id: 5,
+    title: "Wooden Wonders",
+    type: "Front-End",
+    icon: <GiWoodCabin></GiWoodCabin>,
+    url: "",
+    description: "",
+  },
+  {
+    id: 6,
+    title: "Jobster",
+    type: "Full-stack",
+    icon: <IoIosPeople></IoIosPeople>,
+    url: "",
+    description: "",
+  },
+  {
+    id: 7,
+    title: "Love Roses",
+    type: "Front-End",
+    icon: <GiRose></GiRose>,
+    url: "",
+    description: "",
+  },
+
+  {
+    id: 8,
+    title: "The Recipes Room",
+    type: "Front-End",
+    icon: <GiCutLemon></GiCutLemon>,
+    url: "https://the-recipes-room.netlify.app/recipes/",
+    description: "",
+  },
 ];
